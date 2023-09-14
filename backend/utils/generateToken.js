cb0985c14140c6e17936c14e3c2ab10a79787c1f
himanshu1-2 <34888386+himanshu1-2@users.noreply.github.com>
2023-09-15 01:46:46 +0530

    Update generateToken.js

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -7,7 +7,7 @@ const generateToken = (res, userId) => {
 
   // Set JWT as an HTTP-Only cookie
   res.cookie('jwt', token, {
-    httpOnly: true,
+    httpOnly: false,
     secure: true, // Use secure cookies in production
     sameSite: 'strict', // Prevent CSRF attacks
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
